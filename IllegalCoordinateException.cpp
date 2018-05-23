da933ef179f623da50eac2c0d75786b75deb8f3b
GeorgiDg <33101215+Georgidg@users.noreply.github.com>
2018-05-24 01:17:54 +0300

    Update IllegalCoordinateException.cpp

--- IllegalCoordinateException.cpp
+++ IllegalCoordinateException.cpp
@@ -1,10 +1,12 @@
+
+
 #include "IllegalCoordinateException.h"
 
-IllegalCoordinateException::IllegalCoordinateException(int i, int j) {
-	this->i = i;
-	this->j = j;
-}
 
 string IllegalCoordinateException::theCoordinate() const {
-	return (to_string(i) + "," + to_string(j));
+    return (to_string(i) + "," + to_string(j));
+}
+IllegalCoordinateException::IllegalCoordinateException(int i, int j) {
+    this->i = i;
+    this->j = j;
 }
